0000c3c0c7ed86dbb8b85cf9c7fd7613bb4c3d7c
moonicecreamstudio <moonicecreamstudio@gmail.com>
2025-11-13 20:32:06 -0500

    Renamed Scene and added speedup camera

--- ProceduralGeneration/Assets/Scripts/CameraController.cs
+++ ProceduralGeneration/Assets/Scripts/CameraController.cs
@@ -7,12 +7,13 @@ public class CameraController : MonoBehaviour
 
     public GameObject _camera;
     public float _moveSpeed;
-
+    public float _speedUp;
+    private float _speedDefault;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _speedDefault = _moveSpeed;
     }
 
     // Update is called once per frame
@@ -24,6 +25,15 @@ public class CameraController : MonoBehaviour
     void CameraControls()
     {
         Vector3 cameraPosition = _camera.transform.position;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _moveSpeed = _speedUp;
+        }
+        else
+        {
+            _moveSpeed = _speedDefault;
+        }
 
         if (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W)))
         {
